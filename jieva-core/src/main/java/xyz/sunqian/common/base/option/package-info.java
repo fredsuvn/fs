b863c8560c96e99a45f449dd3492e44b6c4cdd37
fredsuvn <fredsuvn@163.com>
2025-03-26 23:23:43 +0800

    update package-info for option and value

--- jieva-core/src/main/java/xyz/sunqian/common/base/option/package-info.java
+++ jieva-core/src/main/java/xyz/sunqian/common/base/option/package-info.java
@@ -1,4 +1,9 @@
 /**
  * Provides classes and interfaces for options.
+ * <p>
+ * Core interfaces:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.option.Option};</li>
+ * </ul>
  */
 package xyz.sunqian.common.base.option;
--- jieva-core/src/main/java/xyz/sunqian/common/base/value/package-info.java
+++ jieva-core/src/main/java/xyz/sunqian/common/base/value/package-info.java
@@ -1,4 +1,10 @@
 /**
  * Provides classes and interfaces for values and variables.
+ * <p>
+ * Core interfaces:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.value.Val};</li>
+ *     <li>{@link xyz.sunqian.common.base.value.Var};</li>
+ * </ul>
  */
 package xyz.sunqian.common.base.value;
