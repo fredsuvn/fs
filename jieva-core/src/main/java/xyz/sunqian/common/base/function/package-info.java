c6e56ba376012e9742eec0e4b54eaec5c0a69c0d
sunqian <fredsuvn@163.com>
2025-05-10 17:00:04 +0800

    add base.function

---
+++ jieva-core/src/main/java/xyz/sunqian/common/base/function/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * This package provides extended function interfaces. And it doesn't have the default nullable/nonnull configuration.
+ * <p>
+ * Indexed functions:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedFunction}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedIntFunction}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedLongFunction}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedDoubleFunction}</li>
+ * </ul>
+ * Indexed consumers:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedConsumer}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedIntConsumer}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedLongConsumer}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedDoubleConsumer}</li>
+ * </ul>
+ * Indexed predicates:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedPredicate}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedIntPredicate}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedLongPredicate}</li>
+ *     <li>{@link xyz.sunqian.common.base.function.IndexedDoublePredicate}</li>
+ * </ul>
+ */
+package xyz.sunqian.common.base.function;
+
