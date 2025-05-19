624f05e78bd9dcaa6b4d6a4a07121efd82ce76c0
fredsuvn <fredsuvn@163.com>
2025-05-20 01:11:39 +0800

    some tests

---
+++ jieva-core/src/main/java/xyz/sunqian/common/test/SomeException.java
@@ -0,0 +1,11 @@
+package xyz.sunqian.common.test;
+
+public class SomeException extends Exception {
+
+    public SomeException() {
+    }
+
+    public SomeException(Throwable cause) {
+        super(cause);
+    }
+}
