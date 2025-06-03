3ceb295cb12b93d2ae0e6a588a60ea50aa8b4b42
sunqian <fredsuvn@163.com>
2025-06-03 15:15:17 +0800

    optimize proxy

---
+++ jieva-core/src/main/java/xyz/sunqian/common/reflect/proxy/asm/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The package for <a href="https://asm.ow2.io/">ASM</a> proxy implementation.
+ * <p>
+ * Core Implementations:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.reflect.proxy.asm.AsmProxyClassGenerator}</li>
+ * </ul>
+ */
+package xyz.sunqian.common.reflect.proxy.asm;
---
+++ jieva-core/src/main/java/xyz/sunqian/common/reflect/proxy/jdk/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The package for JDK dynamic proxy implementation.
+ * <p>
+ * Core Implementations:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.reflect.proxy.jdk.JdkProxyClassGenerator}</li>
+ * </ul>
+ */
+package xyz.sunqian.common.reflect.proxy.jdk;
