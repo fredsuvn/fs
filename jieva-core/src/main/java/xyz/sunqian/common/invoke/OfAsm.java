f38cd951b8f7d3ac6e02ba6584ae98091cd6d309
sunqian <fredsuvn@163.com>
2025-06-03 17:43:48 +0800

    optimize invoke

---
+++ jieva-core/src/main/java/xyz/sunqian/common/invoke/OfAsm.java
@@ -0,0 +1,19 @@
+// package xyz.sunqian.common.invoke;
+//
+// import xyz.sunqian.annotations.Nonnull;
+//
+// import java.lang.reflect.Constructor;
+// import java.lang.reflect.Method;
+//
+// public class OfAsm implements InvocableGenerator {
+//
+//     @Override
+//     public @Nonnull Invocable generate(@Nonnull Method method) {
+//         return null;
+//     }
+//
+//     @Override
+//     public @Nonnull Invocable generate(@Nonnull Constructor<?> constructor) {
+//         return null;
+//     }
+// }
