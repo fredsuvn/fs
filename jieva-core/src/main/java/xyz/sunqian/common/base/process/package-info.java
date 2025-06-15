61f9ecee3256dad6110185e5672e7366f3173335
sunqian <fredsuvn@163.com>
2025-06-15 22:29:33 +0800

    optimize tests

--- jieva-core/src/main/java/xyz/sunqian/common/base/process/package-info.java
+++ jieva-core/src/main/java/xyz/sunqian/common/base/process/package-info.java
@@ -6,9 +6,10 @@
  *     <li>{@link xyz.sunqian.common.base.process.JieProcess}</li>
  * </ul>
  * <p>
- * Interfaces for process:
+ * Interfaces and classes for process:
  * <ul>
  *     <li>{@link xyz.sunqian.common.base.process.ProcessReceipt}</li>
+ *     <li>{@link xyz.sunqian.common.base.process.VirtualProcess}</li>
  * </ul>
  */
 package xyz.sunqian.common.base.process;
