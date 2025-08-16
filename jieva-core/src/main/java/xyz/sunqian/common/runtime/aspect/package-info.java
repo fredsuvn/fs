3d90061ba0629055d410795fc7dadb9f822135bd
fredsuvn <fredsuvn@163.com>
2025-08-17 01:43:47 +0800

    add aspect package

---
+++ jieva-core/src/main/java/xyz/sunqian/common/runtime/aspect/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Provides utilities and interfaces for aspect-oriented.
+ * <p>
+ * Utilities Kits:
+ * <ul>
+ * </ul>
+ * <p>
+ * Interfaces:
+ * <ul>
+ * </ul>
+ */
+package xyz.sunqian.common.runtime.aspect;
--- jieva-core/src/main/java/xyz/sunqian/common/runtime/package-info.java
+++ jieva-core/src/main/java/xyz/sunqian/common/runtime/package-info.java
@@ -4,8 +4,9 @@
  * Sub Packages:
  * <ul>
  *     <li>{@link xyz.sunqian.common.runtime.reflect}: provides fundamental reflection capabilities;</li>
- *     <li>{@link xyz.sunqian.common.runtime.invoke}: provides a unified invocation interface;</li>
+ *     <li>{@link xyz.sunqian.common.runtime.invoke}: provides unified invocation interfaces and related kit;</li>
  *     <li>{@link xyz.sunqian.common.runtime.proxy}: enables dynamic proxying capabilities;</li>
+ *     <li>{@link xyz.sunqian.common.runtime.aspect}: implements aspect-oriented features;</li>
  * </ul>
  */
 package xyz.sunqian.common.runtime;
