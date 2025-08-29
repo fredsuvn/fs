4f8a8b36d8700f70ed0aa194d32a80e86f526801
sunqian <fredsuvn@163.com>
2025-08-29 20:07:06 +0800

    optimize tests for StringView

--- jieva-core/src/test/java/tests/base/string/StringViewTest.java
+++ jieva-core/src/test/java/tests/base/string/StringViewTest.java
@@ -20,8 +20,25 @@ public class StringViewTest implements PrintTest {
     public void testStringView() throws Exception {
         String fullString = "0123456789";
         List<String> strings = ListKit.list("012", "345", "678", "9");
+        testStringView(fullString, strings);
+        testStringView(fullString, ListKit.list("01", "23", "45", "67", "89"));
+        testStringView(fullString, ListKit.list("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        testStringView(fullString, ListKit.list("0", "1", "2", "3", "", "4", "", "5", "", "6", "7", "8", "9"));
+        testStringView(fullString, ListKit.list("0", "1", "2", "3", "", "4", "", "5678", "", "", "9", "", ""));
         StringView sv = StringView.of(strings);
-        assertEquals(sv.length(), 10);
+        expectThrows(IndexOutOfBoundsException.class, () -> sv.charAt(-1));
+        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(-1, 5));
+        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(0, 11));
+        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(6, 5));
+        Method findNode = sv.getClass().getDeclaredMethod("findNode", int.class);
+        findNode.setAccessible(true);
+        Throwable invokeEx = expectThrows(InvocationTargetException.class, () -> findNode.invoke(sv, 100));
+        assertTrue(invokeEx.getCause() instanceof UnreachablePointException);
+    }
+
+    private void testStringView(String fullString, List<String> strings) {
+        StringView sv = StringView.of(strings);
+        assertEquals(sv.length(), fullString.length());
         assertEquals(sv.toString(), fullString);
         for (int i = 0; i < 10; i++) {
             assertEquals(sv.charAt(i), i + '0');
@@ -31,13 +48,5 @@ public class StringViewTest implements PrintTest {
                 assertEquals(sv.subSequence(start, end).toString(), fullString.subSequence(start, end).toString());
             }
         }
-        expectThrows(IndexOutOfBoundsException.class, () -> sv.charAt(-1));
-        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(-1, 5));
-        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(0, 11));
-        expectThrows(IndexOutOfBoundsException.class, () -> sv.subSequence(6, 5));
-        Method findNode = sv.getClass().getDeclaredMethod("findNode", int.class);
-        findNode.setAccessible(true);
-        Throwable invokeEx = expectThrows(InvocationTargetException.class, () -> findNode.invoke(sv, 100));
-        assertTrue(invokeEx.getCause() instanceof UnreachablePointException);
     }
 }
