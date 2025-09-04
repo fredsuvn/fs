462ec5cf367a7b7a5d7522031290758afe3da31d
sunqian <fredsuvn@163.com>
2025-09-04 22:56:05 +0800

    add package-info for base

---
+++ jieva-core/src/main/java/xyz/sunqian/common/base/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * This package is the base package for this lib, provides the most basic utilities:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.Jie}</li>
+ *     <li>{@link xyz.sunqian.common.base.CheckKit}</li>
+ * </ul>
+ * And, it also has many subpackages provide very basic utilities, interfaces and classes:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.base.bytes}</li>
+ *     <li>{@link xyz.sunqian.common.base.chars}</li>
+ *     <li>{@link xyz.sunqian.common.base.exception}</li>
+ *     <li>{@link xyz.sunqian.common.base.lang}</li>
+ *     <li>{@link xyz.sunqian.common.base.logging}</li>
+ *     <li>{@link xyz.sunqian.common.base.math}</li>
+ *     <li>{@link xyz.sunqian.common.base.option}</li>
+ *     <li>{@link xyz.sunqian.common.base.process}</li>
+ *     <li>{@link xyz.sunqian.common.base.random}</li>
+ *     <li>{@link xyz.sunqian.common.base.res}</li>
+ *     <li>{@link xyz.sunqian.common.base.string}</li>
+ *     <li>{@link xyz.sunqian.common.base.system}</li>
+ *     <li>{@link xyz.sunqian.common.base.thread}</li>
+ *     <li>{@link xyz.sunqian.common.base.time}</li>
+ *     <li>{@link xyz.sunqian.common.base.value}</li>
+ * </ul>
+ */
+package xyz.sunqian.common.base;
