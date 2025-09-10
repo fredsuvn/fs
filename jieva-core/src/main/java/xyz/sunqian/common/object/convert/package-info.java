f3e4525de98c36d870e369ed623acd41d505fe10
sunqian <fredsuvn@163.com>
2025-09-10 16:38:10 +0800

    optimize comments for object.convert

---
+++ jieva-core/src/main/java/xyz/sunqian/common/object/convert/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * Package for object conversions.
+ * <p>
+ * Supports copying object properties, including instances of {@link java.util.Map} and non-map objects, using code
+ * similar to the following:
+ * <pre>{@code
+ * DataMapper dataMapper = DataMapper.defaultMapper();
+ * dataMapper.copyProperties(src, dst, options);
+ * }</pre>
+ * <p>
+ * The core interfaces and classes for data mapping:
+ * <ul>
+ *     <li>{@link xyz.sunqian.common.object.convert.DataMapper}</li>
+ *     <li>{@link xyz.sunqian.common.object.convert.MappingOptions}</li>
+ * </ul>
+ * Supports object type conversion, using code similar to the following:
+ * <pre>{@code
+ *
+ * }</pre>
+ * <p>
+ * The core interfaces and classes for object type conversion:
+ * <ul>
+ *     <li></li>
+ * </ul>
+ */
+package xyz.sunqian.common.object.convert;
--- jieva-core/src/main/java/xyz/sunqian/common/object/package-info.java
+++ jieva-core/src/main/java/xyz/sunqian/common/object/package-info.java
@@ -1,8 +1,8 @@
 /**
- * Provides interfaces and utilities for data objects.
- * <p>
- * Utilities:
+ * This package and its subpackages provide various object related operations;
  * <ul>
+ *     <li>{@link xyz.sunqian.common.object.data}: Parses and manipulates objects as data entities;</li>
+ *     <li>{@link xyz.sunqian.common.object.convert}: Converts objects into different types;</li>
  * </ul>
  */
 package xyz.sunqian.common.object;
