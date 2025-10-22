4d46a9365e42fa05112c2fbde750e44b231283e2
fredsuvn <fredsuvn@163.com>
2025-10-22 19:45:25 +0800

    add test for app package

---
+++ jieva-core/src/test/java/tests/app/AppTest.java
@@ -0,0 +1,37 @@
+package tests.app;
+
+import org.testng.annotations.Test;
+import xyz.sunqian.common.app.SimpleApp;
+import xyz.sunqian.common.app.SimpleAppException;
+import xyz.sunqian.test.PrintTest;
+
+import static org.testng.Assert.expectThrows;
+
+public class AppTest implements PrintTest {
+
+    @Test
+    public void testSimpleApp() throws Exception {
+        SimpleApp app = () -> {
+        };
+        app.shutdown();
+    }
+
+    @Test
+    public void testException() throws Exception {
+        {
+            // SimpleAppException
+            expectThrows(SimpleAppException.class, () -> {
+                throw new SimpleAppException();
+            });
+            expectThrows(SimpleAppException.class, () -> {
+                throw new SimpleAppException("");
+            });
+            expectThrows(SimpleAppException.class, () -> {
+                throw new SimpleAppException("", new RuntimeException());
+            });
+            expectThrows(SimpleAppException.class, () -> {
+                throw new SimpleAppException(new RuntimeException());
+            });
+        }
+    }
+}
