3ef016dadc949f45f22a0e037274496758f15d9e
fredsuvn <fredsuvn@163.com>
2025-11-07 00:38:46 +0800

    remove testjvm package

--- kitva-core/src/main/java/space/sunqian/common/testjvm/TestJvm.java
+++
@@ -1,9 +0,0 @@
-package space.sunqian.common.testjvm;
-
-public class TestJvm {
-
-    public static void testJvm() {
-        TestJvmService testJvmService = TestJvmService.Back.INST;
-        testJvmService.testJvm();
-    }
-}
