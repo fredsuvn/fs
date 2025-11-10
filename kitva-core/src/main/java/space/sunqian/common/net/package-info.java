5818840afc2efb640a616a131c1905070dbdd2d4
fredsuvn <fredsuvn@163.com>
2025-11-11 00:17:33 +0800

    optimize net package document

--- kitva-core/src/main/java/space/sunqian/common/net/package-info.java
+++ kitva-core/src/main/java/space/sunqian/common/net/package-info.java
@@ -1,20 +1,20 @@
 /**
- * Provides utilities and interfaces for network, and subpackages for different protocols and tools.
+ * Provides utilities and interfaces for network, such as Http client, TCP client and server, UDP sender and server.
  * <p>
  * Utilities:
  * <ul>
  *     <li>{@link space.sunqian.common.net.NetKit}</li>
  * </ul>
- * Core interfaces:
+ * Core interfaces for client and server:
  * <ul>
- *     <li>{@link space.sunqian.common.net.NetServer}</li>
  *     <li>{@link space.sunqian.common.net.NetClient}</li>
+ *     <li>{@link space.sunqian.common.net.NetServer}</li>
  * </ul>
- * Subpackages:
+ * Subpackages for http, tcp and udp:
  * <ul>
+ *     <li>{@link space.sunqian.common.net.http}</li>
  *     <li>{@link space.sunqian.common.net.tcp}</li>
  *     <li>{@link space.sunqian.common.net.udp}</li>
- *     <li>{@link space.sunqian.common.net.http}</li>
  * </ul>
  */
 package space.sunqian.common.net;
