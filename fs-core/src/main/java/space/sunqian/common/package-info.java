8cf20f7ba5a89564a87a2d4a863efdad9c455776
fredsuvn <fredsuvn@163.com>
2025-11-14 19:10:07 +0800

    optimize javadoc

--- fs-core/src/main/java/space/sunqian/common/package-info.java
+++ fs-core/src/main/java/space/sunqian/common/package-info.java
@@ -6,5 +6,19 @@
  *     <li>{@link space.sunqian.common.FsLoader}: the loader for implementations of fs;</li>
  * </ul>
  * And sub-packages for specific functions:
+ * <ul>
+ *     <li>{@link space.sunqian.common.base}</li>
+ *     <li>{@link space.sunqian.common.app}</li>
+ *     <li>{@link space.sunqian.common.cache}</li>
+ *     <li>{@link space.sunqian.common.codec}</li>
+ *     <li>{@link space.sunqian.common.collect}</li>
+ *     <li>{@link space.sunqian.common.concurrent}</li>
+ *     <li>{@link space.sunqian.common.io}</li>
+ *     <li>{@link space.sunqian.common.net}</li>
+ *     <li>{@link space.sunqian.common.object}</li>
+ *     <li>{@link space.sunqian.common.random}</li>
+ *     <li>{@link space.sunqian.common.runtime}</li>
+ *     <li>{@link space.sunqian.common.third}</li>
+ * </ul>
  */
 package space.sunqian.common;
