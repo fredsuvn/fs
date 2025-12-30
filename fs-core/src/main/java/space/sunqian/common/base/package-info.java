4253879499a8b5137e59290dd010c314183dff93
fredsuvn <fredsuvn@163.com>
2025-12-31 02:06:10 +0800

    optimize package-info for base

--- fs-core/src/main/java/space/sunqian/common/base/package-info.java
+++ fs-core/src/main/java/space/sunqian/common/base/package-info.java
@@ -8,32 +8,30 @@
  * <p>
  * Extensions for language base:
  * <ul>
- *     <li>{@link space.sunqian.common.base.coding}</li>
- *     <li>{@link space.sunqian.common.base.enums}</li>
- *     <li>{@link space.sunqian.common.base.exception}</li>
+ *     <li>{@link space.sunqian.common.base.lang}</li>
  * </ul>
  * <p>
- * Extensions for function and value:
+ * Extensions for function, value and option:
  * <ul>
  *     <li>{@link space.sunqian.common.base.function}</li>
  *     <li>{@link space.sunqian.common.base.value}</li>
+ *     <li>{@link space.sunqian.common.base.option}</li>
  * </ul>
  * <p>
  * Base utilities:
  * <ul>
  *     <li>{@link space.sunqian.common.base.bytes}</li>
  *     <li>{@link space.sunqian.common.base.chars}</li>
+ *     <li>{@link space.sunqian.common.base.date}</li>
+ *     <li>{@link space.sunqian.common.base.exception}</li>
  *     <li>{@link space.sunqian.common.base.logging}</li>
  *     <li>{@link space.sunqian.common.base.math}</li>
  *     <li>{@link space.sunqian.common.base.number}</li>
- *     <li>{@link space.sunqian.common.base.option}</li>
  *     <li>{@link space.sunqian.common.base.process}</li>
- *     <li>{@link space.sunqian.common.base.res}</li>
+ *     <li>{@link space.sunqian.common.base.random}</li>
  *     <li>{@link space.sunqian.common.base.string}</li>
  *     <li>{@link space.sunqian.common.base.system}</li>
  *     <li>{@link space.sunqian.common.base.thread}</li>
- *     <li>{@link space.sunqian.common.base.date}</li>
- *     <li>{@link space.sunqian.common.base.random}</li>
  * </ul>
  */
 package space.sunqian.common.base;
