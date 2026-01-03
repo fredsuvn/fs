189bfd80c9ae0da2df44e7207c995a4c963aea62
fredsuvn <fredsuvn@163.com>
2026-01-03 16:23:37 +0800

    fix package-info docs for object and third

--- fs-core/src/main/java/space/sunqian/fs/object/package-info.java
+++ fs-core/src/main/java/space/sunqian/fs/object/package-info.java
@@ -1,6 +1,7 @@
 /**
  * This package and its subpackages provide various object related operations;
  * <ul>
+ *     <li>{@link space.sunqian.fs.object.ObjectKit}: Object utilities;</li>
  *     <li>{@link space.sunqian.fs.object.data}: Parses and manipulates objects as data entities;</li>
  *     <li>{@link space.sunqian.fs.object.convert}: Converts objects into different types;</li>
  * </ul>
