a5d0fb94a78626fa1632a6896f5edf89fcc95a36
fredsuvn <fredsuvn@163.com>
2026-01-09 00:51:54 +0800

    update release.yml and build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,6 @@ tasks.register("printVersion") {
   group = "version"
   description = "Prints the project version"
   doLast {
-    println(project.version)
+    println("version=${project.version}")
   }
 }
