f43ffca321fadb4aae6e5f03ad19d1302c24d6ed
fredsuvn <fredsuvn@163.com>
2026-01-09 00:26:15 +0800

    make AutoVersion un-documented

--- fs-build/src/main/kotlin/space/sunqian/fs/build/processing/AutoVersion.java
+++ fs-build/src/main/kotlin/space/sunqian/fs/build/processing/AutoVersion.java
@@ -1,6 +1,5 @@
 package space.sunqian.fs.build.processing;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,7 +10,7 @@ import java.lang.annotation.Target;
  *
  * @author sunqian
  */
-@Documented
+//@Documented
 @Retention(RetentionPolicy.SOURCE)
 @Target({
     ElementType.FIELD,
