2aba0eba291e7d057f4a9fe78c3213f5f736c414
fredsuvn <fredsuvn@163.com>
2026-01-30 18:28:02 +0800

    add implementation for PropertiesData

--- fs-core/src/main/java/space/sunqian/fs/data/properties/PropertiesData.java
+++ fs-core/src/main/java/space/sunqian/fs/data/properties/PropertiesData.java
@@ -1,4 +1,238 @@
 package space.sunqian.fs.data.properties;
 
-public interface PropertiesData {
+import space.sunqian.annotation.Nonnull;
+import space.sunqian.annotation.Nullable;
+import space.sunqian.fs.Fs;
+import space.sunqian.fs.base.chars.CharsKit;
+import space.sunqian.fs.data.OutputableData;
+import space.sunqian.fs.io.IOKit;
+import space.sunqian.fs.io.IORuntimeException;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * The interface to access properties data.
+ *
+ * @author sunqian
+ */
+public interface PropertiesData extends OutputableData {
+
+    /**
+     * Loads properties from the given input stream, using {@link CharsKit#defaultCharset()}.
+     *
+     * @param in the input stream to read from
+     * @return a new {@link PropertiesData} wraps the loaded properties
+     * @throws IORuntimeException if an I/O error occurs
+     */
+    static @Nonnull PropertiesData load(@Nonnull InputStream in) throws IORuntimeException {
+        return load(in, CharsKit.defaultCharset());
+    }
+
+    /**
+     * Loads properties from the given input stream, using the specified charset.
+     *
+     * @param in      the input stream to read from
+     * @param charset the charset to use
+     * @return a new {@link PropertiesData} wraps the loaded properties
+     * @throws IORuntimeException if an I/O error occurs
+     */
+    static @Nonnull PropertiesData load(@Nonnull InputStream in, @Nonnull Charset charset) throws IORuntimeException {
+        return Fs.uncheck(() -> {
+            Reader reader = IOKit.newReader(in, charset);
+            Properties properties = new Properties();
+            properties.load(reader);
+            return wrap(properties);
+        }, IORuntimeException::new);
+    }
+
+    /**
+     * Returns a new {@link PropertiesData} wraps the given {@link Properties}.
+     *
+     * @param properties the {@link Properties} to wrap
+     * @return a new {@link PropertiesData} wraps the given {@link Properties}
+     */
+    static @Nonnull PropertiesData wrap(@Nonnull Properties properties) {
+        return () -> properties;
+    }
+
+    /**
+     * Gets the string value of the property, or {@code null} if not found.
+     *
+     * @param name the name of the property
+     * @return the string value of the property, or {@code null} if not found
+     */
+    default @Nullable String getString(@Nonnull String name) {
+        Object value = asMap().get(name);
+        return value == null ? null : value.toString();
+    }
+
+    /**
+     * Gets the string value of the property, or {@code defaultValue} if not found.
+     *
+     * @param name         the name of the property
+     * @param defaultValue the default value
+     * @return the string value of the property, or {@code defaultValue} if not found
+     */
+    default @Nonnull String getString(@Nonnull String name, @Nonnull String defaultValue) {
+        String value = getString(name);
+        return value == null ? defaultValue : value;
+    }
+
+    /**
+     * Gets the int value of the property, or {@code 0} if not found.
+     *
+     * @param name the name of the property
+     * @return the int value of the property, or {@code 0} if not found
+     */
+    default int getInt(@Nonnull String name) {
+        return getInt(name, 0);
+    }
+
+    /**
+     * Gets the int value of the property, or {@code defaultValue} if not found.
+     *
+     * @param name         the name of the property
+     * @param defaultValue the default value
+     * @return the int value of the property, or {@code defaultValue} if not found
+     */
+    default int getInt(@Nonnull String name, int defaultValue) {
+        String value = getString(name);
+        return value == null ? defaultValue : Integer.parseInt(value);
+    }
+
+    /**
+     * Gets the long value of the property, or {@code 0} if not found.
+     *
+     * @param name the name of the property
+     * @return the long value of the property, or {@code 0} if not found
+     */
+    default long getLong(@Nonnull String name) {
+        return getLong(name, 0);
+    }
+
+    /**
+     * Gets the long value of the property, or {@code defaultValue} if not found.
+     *
+     * @param name         the name of the property
+     * @param defaultValue the default value
+     * @return the long value of the property, or {@code defaultValue} if not found
+     */
+    default long getLong(@Nonnull String name, long defaultValue) {
+        String value = getString(name);
+        return value == null ? defaultValue : Long.parseLong(value);
+    }
+
+    /**
+     * Gets the float value of the property, or {@code 0.0f} if not found.
+     *
+     * @param name the name of the property
+     * @return the float value of the property, or {@code 0.0f} if not found
+     */
+    default float getFloat(@Nonnull String name) {
+        return getFloat(name, 0.0f);
+    }
+
+    /**
+     * Gets the float value of the property, or {@code defaultValue} if not found.
+     *
+     * @param name         the name of the property
+     * @param defaultValue the default value
+     * @return the float value of the property, or {@code defaultValue} if not found
+     */
+    default float getFloat(@Nonnull String name, float defaultValue) {
+        String value = getString(name);
+        return value == null ? defaultValue : Float.parseFloat(value);
+    }
+
+    /**
+     * Gets the double value of the property, or {@code 0.0} if not found.
+     *
+     * @param name the name of the property
+     * @return the double value of the property, or {@code 0.0} if not found
+     */
+    default double getDouble(@Nonnull String name) {
+        return getDouble(name, 0.0);
+    }
+
+    /**
+     * Gets the double value of the property, or {@code defaultValue} if not found.
+     *
+     * @param name         the name of the property
+     * @param defaultValue the default value
+     * @return the double value of the property, or {@code defaultValue} if not found
+     */
+    default double getDouble(@Nonnull String name, double defaultValue) {
+        String value = getString(name);
+        return value == null ? defaultValue : Double.parseDouble(value);
+    }
+
+    /**
+     * Returns {@code true} if the property value is one of "true", "1", "yes", "y", or "enabled" (case-insensitive),
+     * otherwise {@code false}.
+     *
+     * @param name the name of the property
+     * @return {@code true} if the property value is one of "true", "1", "yes", "y", or "enabled" (case-insensitive),
+     * otherwise {@code false}.
+     */
+    default boolean getBoolean(String name) {
+        String value = getString(name);
+        return value != null && (
+            value.equalsIgnoreCase("true")
+                || value.equals("1")
+                || value.equalsIgnoreCase("yes")
+                || value.equalsIgnoreCase("y")
+                || value.equalsIgnoreCase("enabled")
+        );
+    }
+
+    /**
+     * Sets the value of the property.
+     *
+     * @param name  the name of the property
+     * @param value the value to set
+     */
+    default void set(@Nonnull String name, @Nonnull Object value) {
+        asMap().put(name, value);
+    }
+
+    /**
+     * Removes the property.
+     *
+     * @param name the name of the property
+     */
+    default void remove(@Nonnull String name) {
+        asMap().remove(name);
+    }
+
+    /**
+     * Returns a properties view of the properties, any changes to the properties will be reflected in the map, and vice
+     * versa.
+     *
+     * @return a properties view of the properties
+     */
+    @Nonnull
+    Properties asProperties();
+
+    /**
+     * Returns a map view of the properties, any changes to the map will be reflected in the properties, and vice
+     * versa.
+     *
+     * @return a map view of the properties
+     */
+    default @Nonnull Map<@Nonnull String, @Nonnull Object> asMap() {
+        return Fs.as(asProperties());
+    }
+
+    @Override
+    default void writeTo(@Nonnull OutputStream out, @Nonnull Charset charset) throws IORuntimeException {
+        Properties properties = asProperties();
+        Writer writer = IOKit.newWriter(out, charset);
+        Fs.uncheck(() -> properties.store(writer, null), IORuntimeException::new);
+    }
 }
--- fs-core/src/test/java/tests/data/properties/PropertiesTest.java
+++ fs-core/src/test/java/tests/data/properties/PropertiesTest.java
@@ -1,11 +1,94 @@
 package tests.data.properties;
 
+import internal.test.PrintTest;
 import org.junit.jupiter.api.Test;
+import space.sunqian.fs.base.chars.CharsKit;
+import space.sunqian.fs.base.system.ResKit;
+import space.sunqian.fs.data.properties.PropertiesData;
 import space.sunqian.fs.data.properties.PropertiesException;
+import space.sunqian.fs.io.IOKit;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Reader;
+import java.nio.channels.Channels;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class PropertiesTest {
+public class PropertiesTest implements PrintTest {
+
+    @Test
+    public void testPropertiesData() throws Exception {
+        {
+            // common
+            PropertiesData properties = PropertiesData.load(ResKit.findResource("data/x.properties").openStream());
+            printFor("x.properties", properties.asMap());
+            assertEquals(1, properties.getInt("x1"));
+            assertEquals(100, properties.getInt("x100", 100));
+            assertEquals(2L, properties.getLong("x2"));
+            assertEquals(200L, properties.getLong("x200", 200L));
+            assertEquals(3.3f, properties.getFloat("x3"));
+            assertEquals(300.0f, properties.getFloat("x300", 300.0f));
+            assertEquals(4.4, properties.getDouble("x4"));
+            assertEquals(400.0, properties.getDouble("x400", 400.0));
+            assertEquals(true, properties.getBoolean("x5"));
+            assertEquals(false, properties.getBoolean("x6"));
+            assertEquals("hello", properties.getString("x7"));
+            assertEquals("world", properties.getString("x700", "world"));
+            assertEquals("hello", properties.getString("x7", "world"));
+            assertEquals(false, properties.getBoolean("x7"));
+            assertEquals(true, properties.getBoolean("x8"));
+            assertEquals(true, properties.getBoolean("x9"));
+            assertEquals(true, properties.getBoolean("x10"));
+            assertEquals(false, properties.getBoolean("x11"));
+            assertEquals(false, properties.getBoolean("x1100"));
+            assertEquals(true, properties.getBoolean("x1"));
+            assertEquals(null, properties.getString("x100"));
+            assertEquals("中文", properties.getString("x12"));
+
+            // update
+            properties.set("x1", "1000");
+            assertEquals(1000, properties.getInt("x1"));
+            properties.set("x100", "1000");
+            assertEquals(1000, properties.getInt("x100"));
+            properties.remove("x2");
+            assertNull(properties.getString("x2"));
+            printFor("x.properties - modified", properties.asMap());
+
+            // write
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            properties.writeTo(output);
+            checkOutput(output);
+            output.reset();
+            properties.writeTo(Channels.newChannel(output));
+            checkOutput(output);
+            output.reset();
+        }
+    }
+
+    private void checkOutput(ByteArrayOutputStream output) throws Exception {
+        Properties properties = new Properties();
+        Reader reader = IOKit.newReader(new ByteArrayInputStream(output.toByteArray()), CharsKit.defaultCharset());
+        properties.load(reader);
+        assertEquals("1000", properties.getProperty("x1"));
+        assertNull(properties.getProperty("x2"));
+        assertEquals("3.3", properties.getProperty("x3"));
+        assertEquals("4.4", properties.getProperty("x4"));
+        assertEquals("true", properties.getProperty("x5"));
+        assertEquals("false", properties.getProperty("x6"));
+        assertEquals("hello", properties.getProperty("x7"));
+        assertEquals("yes", properties.getProperty("x8"));
+        assertEquals("Y", properties.getProperty("x9"));
+        assertEquals("Enabled", properties.getProperty("x10"));
+        assertEquals("Disabled", properties.getProperty("x11"));
+        assertEquals("1000", properties.getProperty("x100"));
+        assertEquals("中文", properties.getProperty("x12"));
+        assertEquals(12, properties.size());
+        printFor("x.properties - output", properties);
+    }
 
     @Test
     public void testException() throws Exception {
