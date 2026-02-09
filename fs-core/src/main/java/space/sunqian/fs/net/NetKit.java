038dc49bdfdea04823c3f7ce6935ea7f11b1c1f0
fredsuvn <fredsuvn@163.com>
2026-02-09 12:50:26 +0800

    optimize net for broadcast address

--- fs-core/src/main/java/space/sunqian/fs/net/NetKit.java
+++ fs-core/src/main/java/space/sunqian/fs/net/NetKit.java
@@ -1,5 +1,6 @@
 package space.sunqian.fs.net;
 
+import space.sunqian.annotation.Immutable;
 import space.sunqian.annotation.Nonnull;
 import space.sunqian.fs.Fs;
 import space.sunqian.fs.collect.CollectKit;
@@ -9,8 +10,10 @@ import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Network utilities.
@@ -18,6 +21,23 @@ import java.util.function.Predicate;
  * @author sunqian
  */
 public class NetKit {
+
+    /**
+     * The IP address: {@code 255.255.255.255}. This is the default broadcast IP.
+     */
+    public static final @Nonnull InetAddress INET_ADDR_255_255_255_255;
+
+    /**
+     * The IP address: {@code 0.0.0.0}.
+     */
+    public static final @Nonnull InetAddress INET_ADDR_0_0_0_0;
+
+    static {
+        INET_ADDR_255_255_255_255 =
+            Fs.uncheck(() -> InetAddress.getByName("255.255.255.255"), NetException::new);
+        INET_ADDR_0_0_0_0 =
+            Fs.uncheck(() -> InetAddress.getByName("0.0.0.0"), NetException::new);
+    }
 
     /**
      * Returns a broadcast IP address, if the local broadcast IP is not found, returns {@code 255.255.255.255}.
@@ -30,10 +50,29 @@ public class NetKit {
     }
 
     private static @Nonnull InetAddress getBroadcastAddress0() throws Exception {
+        return allBroadcastAddresses0()
+            .stream()
+            .findFirst()
+            .orElse(INET_ADDR_255_255_255_255);
+    }
+
+    /**
+     * Returns all broadcast IP address, if the local broadcast IP is not found, returns an empty list.
+     *
+     * @return all broadcast IP address, if the local broadcast IP is not found, returns an empty list
+     * @throws NetException if an error occurs
+     */
+    public static @Nonnull @Immutable List<@Nonnull InetAddress> allBroadcastAddresses() throws NetException {
+        return Fs.uncheck(NetKit::allBroadcastAddresses0, NetException::new);
+    }
+
+    private static @Nonnull @Immutable List<@Nonnull InetAddress> allBroadcastAddresses0() throws Exception {
         Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
         return StreamKit.stream(() -> CollectKit.asIterator(interfaces))
             .filter(networkInterface ->
                 Fs.uncheck(networkInterface::isUp, NetException::new))
+            .filter(networkInterface ->
+                Fs.uncheck(networkInterface::supportsMulticast, NetException::new))
             .filter(((Predicate<NetworkInterface>) networkInterface ->
                 Fs.uncheck(networkInterface::isLoopback, NetException::new)).negate())
             .filter(((Predicate<NetworkInterface>) networkInterface ->
@@ -41,8 +80,8 @@ public class NetKit {
             .flatMap(networkInterface -> networkInterface.getInterfaceAddresses().stream())
             .map(InterfaceAddress::getBroadcast)
             .filter(Objects::nonNull)
-            .findFirst()
-            .orElse(InetAddress.getByName("255.255.255.255"));
+            //.filter(ip -> !ip.equals(INET_ADDR_0_0_0_0))
+            .collect(Collectors.toList());
     }
 
     private NetKit() {
--- fs-core/src/test/java/tests/net/NetTest.java
+++ fs-core/src/test/java/tests/net/NetTest.java
@@ -3,10 +3,17 @@ package tests.net;
 import internal.test.PrintTest;
 import org.junit.jupiter.api.Test;
 import space.sunqian.fs.net.NetException;
+import space.sunqian.fs.net.NetKit;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NetTest implements PrintTest {
+
+    @Test
+    public void testBroadcastAddress() throws Exception {
+        printFor("allBroadcastAddresses", NetKit.allBroadcastAddresses());
+        printFor("getBroadcastAddress", NetKit.getBroadcastAddress());
+    }
 
     @Test
     public void testException() throws Exception {
