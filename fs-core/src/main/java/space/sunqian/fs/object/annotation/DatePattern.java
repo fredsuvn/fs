c5e47d0652d2d3c6dcf2da9eb00d6d9737153c63
fredsuvn <fredsuvn@163.com>
2026-02-24 01:38:32 +0800

    add annotation for object

---
+++ fs-core/src/main/java/space/sunqian/fs/object/annotation/DatePattern.java
@@ -0,0 +1,44 @@
+package space.sunqian.fs.object.annotation;
+
+import space.sunqian.fs.base.date.DateKit;
+import space.sunqian.fs.object.convert.ObjectConverter;
+import space.sunqian.fs.object.convert.ObjectCopier;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.time.ZoneId;
+
+/**
+ * This annotation is used to specify the date pattern to be used. It is typically available for the default
+ * {@link ObjectConverter} and {@link ObjectCopier} and their handlers when converting to a non-map object.
+ *
+ * @author sunqian
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+    ElementType.METHOD,
+    ElementType.FIELD,
+    ElementType.PARAMETER,
+    ElementType.LOCAL_VARIABLE,
+    ElementType.TYPE_USE,
+})
+public @interface DatePattern {
+
+    /**
+     * The date pattern to be used, default is {@link DateKit#DEFAULT_PATTERN}.
+     *
+     * @return the date pattern to be used, default is {@link DateKit#DEFAULT_PATTERN}
+     */
+    String value() default DateKit.DEFAULT_PATTERN;
+
+    /**
+     * The zone id to be used, default is {@link ZoneId#systemDefault()}.
+     *
+     * @return the zone id to be used, default is {@link ZoneId#systemDefault()}
+     */
+    String zoneId() default "";
+}
---
+++ fs-core/src/main/java/space/sunqian/fs/object/annotation/NumPattern.java
@@ -0,0 +1,36 @@
+package space.sunqian.fs.object.annotation;
+
+import space.sunqian.fs.base.number.NumKit;
+import space.sunqian.fs.object.convert.ObjectConverter;
+import space.sunqian.fs.object.convert.ObjectCopier;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to specify the number pattern to be used. It is typically available for the default
+ * {@link ObjectConverter} and {@link ObjectCopier} and their handlers when converting to a non-map object.
+ *
+ * @author sunqian
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+    ElementType.METHOD,
+    ElementType.FIELD,
+    ElementType.PARAMETER,
+    ElementType.LOCAL_VARIABLE,
+    ElementType.TYPE_USE,
+})
+public @interface NumPattern {
+
+    /**
+     * The number pattern to be used, default is {@link NumKit#DEFAULT_PATTERN}.
+     *
+     * @return the number pattern to be used, default is {@link NumKit#DEFAULT_PATTERN}
+     */
+    String value() default NumKit.DEFAULT_PATTERN;
+}
