239038e75442c527e5395ac51467a93d5fb18adc
fredsuvn <fredsuvn@163.com>
2026-03-05 11:06:35 +0800

    remove SimpleAppender

--- fs-core/src/main/java/space/sunqian/fs/base/chars/package-info.java
+++ fs-core/src/main/java/space/sunqian/fs/base/chars/package-info.java
@@ -8,7 +8,6 @@
  * Classes:
  * <ul>
  *     <li>{@link space.sunqian.fs.base.chars.CharsBuilder}</li>
- *     <li>{@link space.sunqian.fs.base.chars.SimpleAppender}</li>
  * </ul>
  */
 package space.sunqian.fs.base.chars;
