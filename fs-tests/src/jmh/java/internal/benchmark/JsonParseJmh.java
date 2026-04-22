8d68666ea8ab8a091b954b94daac9ba9ef0bf547
fredsuvn <fredsuvn@163.com>
2026-04-23 01:14:26 +0800

    fix jmh

--- fs-tests/src/jmh/java/internal/benchmark/JsonParseJmh.java
+++ fs-tests/src/jmh/java/internal/benchmark/JsonParseJmh.java
@@ -26,9 +26,9 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode({Mode.Throughput})
-@Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(1)
+@Warmup(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
+@Fork(5)
 public class JsonParseJmh {
 
     private final TestJsonData data = new TestJsonData();
